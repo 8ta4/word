c5874d63acf9bef6551efe36ffd42f18a8489445
a <a@as-Virtual-Machine.local>
2026-05-06 18:58:14 -0700

    Add opts.styles table with initial “casual” style configuration

--- template.lua
+++ template.lua
@@ -1,4 +1,12 @@
 return {
 	"8ta4/word",
 	dir = "{{dir}}",
+	opts = {
+		styles = {
+			{
+				name = "casual",
+				prompt = "Use a casual tone. Use correct grammar. Check if the sentence meets these requirements. Explain any failures. Give two rewrites of the sentence. Do not rewrite the context.",
+			},
+		},
+	},
 }
