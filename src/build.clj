6cb8f96a2f0370f4ab79efedc1f04145a76cfb18
a <a@as-Virtual-Machine.local>
2026-05-20 17:58:02 -0700

    Introduce configure-plugin helper to render template.lua into Neovim plugin path

--- src/build.clj
+++ src/build.clj
@@ -1,9 +1,19 @@
 (ns build
-  (:require [clojure.java.shell :refer [sh]]))
+  (:require [clojure.java.shell :refer [sh]]
+            [clojure.string :as string]
+            [babashka.fs :refer [expand-home file]]))
+
+(defn configure-plugin
+  []
+  (->> "DEVENV_ROOT"
+       System/getenv
+       (string/replace (slurp "template.lua") "{{dir}}")
+       (spit (file (expand-home "~/.config/nvim/lua/plugins/devenv.lua")))))
 
 (defn build
   {:shadow.build/stage :flush}
   [state]
+  (configure-plugin)
   (println "Updating Neovim remote plugins.")
   (sh "nvim" "--headless" "+Lazy load word" "+UpdateRemotePlugins" "+qa!")
   state)
