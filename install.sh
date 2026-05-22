08bb494881ee72976f741bb2e6e28935fbccb00e
a <a@as-Virtual-Machine.local>
2026-05-22 03:03:09 -0700

    Bump installer

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 set -e
 FILE="word.tar.gz"
 trap 'rm -f "$FILE"' EXIT
-curl -L https://github.com/8ta4/word/releases/download/v0.1.5/word.tar.gz -o $FILE
-echo "39dfc00f0c8f548224f82225ecb1743a8ae83f13dee8a0e41f7740f53c0438b2  $FILE" | shasum -a 256 -c
+curl -L https://github.com/8ta4/word/releases/download/v0.1.6/word.tar.gz -o $FILE
+echo "baa8419c148e452fee042fdf38d7da24779c8620dd2d3b14946a98b52abefac0  $FILE" | shasum -a 256 -c
 tar -xzf $FILE
